efb1a976efaa29fa28d70b24b6e8ea8a64b8bb0f
Edward Hong <edward.hong527@gmail.com>
2020-07-15 21:21:26 +1200

    S04L16 Enabling Cookies

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 
-const { mongoURI } = require('./config/keys')
+const { mongoURI, cookieKey } = require('./config/keys')
 
 require('./models/User')
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  }),
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('./services/passport')
 require('./routes/authRoutes')(app)
