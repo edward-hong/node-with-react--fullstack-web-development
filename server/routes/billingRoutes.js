27c9e7e5e3bcefc6cfe66f88b793d4f5bafc2fc4
Edward Hong <edward.hong527@gmail.com>
2020-07-16 23:38:59 +1200

    S08L19 Route-Specific Middlewares

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,12 +2,10 @@ const { stripeSecretKey } = require('../config/keys')
 
 const stripe = require('stripe')(stripeSecretKey)
 
-module.exports = (app) => {
-  app.post('/api/stripe', async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({ error: 'You must log in!' })
-    }
+const requireLogin = require('../middlewares/requireLogin')
 
+module.exports = (app) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
