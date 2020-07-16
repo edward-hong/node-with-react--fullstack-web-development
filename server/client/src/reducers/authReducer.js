d7b9bf20ca050b2308ac87bb441f943e9a26934f
Edward Hong <edward.hong527@gmail.com>
2020-07-16 17:24:19 +1200

    S07L25 AuthReducer Return Values

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-const authReducer = (state = {}, action) => {
-  console.log(action)
-  switch (action.type) {
+import { FETCH_USER } from '../actions/types'
+
+const authReducer = (state = null, { type, payload }) => {
+  switch (type) {
+    case FETCH_USER:
+      return payload || false
     default:
       return state
   }
