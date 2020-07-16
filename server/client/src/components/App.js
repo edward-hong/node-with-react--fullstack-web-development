6b68d5929423f1f37301ab71f4c0414910147e27
Edward Hong <edward.hong527@gmail.com>
2020-07-16 16:01:47 +1200

    S07L13 Route Configuration

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -6,6 +6,14 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => <div>Hi there</div>
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Landing} />
+      </div>
+    </BrowserRouter>
+  </div>
+)
 
 export default App
